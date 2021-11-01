71299f9bcd78cebce95f5cb1ef8b9bc6f05e3b83
ayang097 <56611785+ayang097@users.noreply.github.com>
2021-10-31 17:34:32 -0700

    Update ayang_lab8_pt1.c

--- Lab8_A2D/turnin/ayang_lab8_pt1.c
+++ Lab8_A2D/turnin/ayang_lab8_pt1.c
@@ -3,7 +3,7 @@
  *	Lab Section:
  *	Assignment: Lab #  Exercise #
  *	Exercise Description: [optional - include for your own benefit]
- *
+ *      DEMO:https://youtu.be/4RSy89mEdc4
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
